0f05836e415e0121913913704174ee5b2727346b
Kazuho Oku <kazuhooku@gmail.com>
2019-04-18 13:32:36 +0900

    check if tracepoint is being used, because calling the trace requires a function call on macOS

--- test.c
+++ test.c
@@ -4,8 +4,11 @@
 int main(int argc, char **argv)
 {
     uint64_t i;
-    for (i = 0; i < 5000000000; ++i)
-	TEST_HELLO("hello world\n");
-    TEST_DONE();
+    for (i = 0; i < 5000000000; ++i) {
+        if (TEST_HELLO_ENABLED())
+	    TEST_HELLO("hello world\n");
+    }
+    if (TEST_DONE_ENABLED())
+        TEST_DONE();
     return 0;
 }
